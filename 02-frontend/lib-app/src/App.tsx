280b3c24997939826493fb650216c5f375350d53
Yong Liang <yoliang@ucsd.edu>
2023-01-11 21:50:12 -0800

    implement search

--- 02-frontend/lib-app/src/App.tsx
+++ 02-frontend/lib-app/src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 import { Navbar } from './layout/NavbarAndFooter/Navbar';
 import { Footer } from './layout/NavbarAndFooter/Footer';
 import { HomePage } from './layout/HomePage/HomePage';
+import { SearchBooksPage } from './layout/SearchBooksPage/SearchBooksPage';
 
 export function App() {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
