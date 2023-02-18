7db29b43c60b3113f4dc36324cfeb196cc3257e4
Yong Liang <yoliang@ucsd.edu>
2023-02-17 21:48:21 -0800

    spring boot review

--- 01-backend/spring-boot-library/src/main/java/com/yoliang/springbootlibrary/requestmodels/ReviewRequest.java
+++ 01-backend/spring-boot-library/src/main/java/com/yoliang/springbootlibrary/requestmodels/ReviewRequest.java
@@ -1,2 +1,15 @@
-package com.yoliang.springbootlibrary.requestmodels;public class ReviewRequest {
+package com.yoliang.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+
+    private double rating;
+
+    private Long bookId;
+
+    private Optional<String> reviewDescription;
 }
